65f7a8b24ee29c3d32a77164a39a450cde4f37bb
Vishath Amarasinghe <95088459+VishathAmarasinghe@users.noreply.github.com>
2024-12-26 11:04:39 +0530

    refactor configs

--- src/View/index.tsx
+++ src/View/index.tsx
@@ -6,12 +6,12 @@ const appointmentView = lazy(() => import("./appointment-view"));
 const carePlanView = lazy(() => import("./careplan-view"));
 const EmployeeView = lazy(() => import("./employee-view"));
 const reportView = lazy(() => import("./report-view"));
-const resourceView = lazy(() => import("./resource-view"));
+const resourceView = lazy(() => import("./Resource-view"));
 const incidentView = lazy(() => import("./incident-view"));
 const careGiverDashboardView = lazy(() => import("./careGiver-dashboard-view"));
 const shiftNoteView = lazy(() => import("./ShiftNote-view"));
 const SettingsView = lazy(() => import("./settings-view"));
-const CareGiverInforView = lazy(()=>import("./careGiverInfo-view"));
+const CareGiverInforView = lazy(() => import("./careGiverInfo-view"));
 
 export const View = {
   dashboardView,
