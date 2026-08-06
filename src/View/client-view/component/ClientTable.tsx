1e42200f78e68cfecfa727a8984e2f5e501da434
vishath Amarasinghe <vishathakila2001@gmail.com>
2026-08-06 18:23:43 +0530

    commentout hide checkbox for workers

--- src/View/client-view/component/ClientTable.tsx
+++ src/View/client-view/component/ClientTable.tsx
@@ -48,7 +48,6 @@ function CustomToolbar({
         <GridToolbarColumnsButton />
         <GridToolbarFilterButton />
         <GridToolbarQuickFilter placeholder="Search" />
-        {/* Temporarily hidden: show deactivated clients toggle
         <FormControlLabel
           control={
             <Checkbox
@@ -60,7 +59,6 @@ function CustomToolbar({
           label="Show deactivated clients"
           sx={{ marginLeft: 2 }}
         />
-        */}
       </Stack>
       {selectedCount > 0 && (
         <Button
